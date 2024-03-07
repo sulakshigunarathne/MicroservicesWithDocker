d8fbc1257f434f560735a965a41a803199b68efd
sulakshigunarathne <gunarathneawsm.20@uom.lk>
2024-03-07 13:59:09 +0530

    API gateway added

--- PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9edbadc12e0731322302f6ff0b3366e0aa4c175d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+508f045a9a437030b0d8b1caae5a6a6ff24c518e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
